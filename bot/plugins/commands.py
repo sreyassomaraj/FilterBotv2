8c6c0f7f15e306b6693f68ee5da727ddff9c0a9f
sreyassomaraj <88197685+sreyassomaraj@users.noreply.github.com>
2021-08-26 19:10:49 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '🔆Our Group🔆', url="https://t.me/coolmoviez123"
                                 )
                         ]
                     ]
